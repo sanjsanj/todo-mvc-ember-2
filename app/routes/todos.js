9bd7700a03805114acfac25a627d47666f04f0ca
sanjsanj <sanjsanj@hotmail.com>
2015-06-30 11:20:20 +0100

    can add new todo

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return this.store.find('todo');
+  },
+  actions: {
+    createTodo: function(newTitle){
+      var todo = this.store.createRecord('todo', {
+        title: newTitle,
+        isCompleted: false
+      });
+      this.controllerFor('todos').set('newTitle', '');
+      todo.save();
+    }
   }
 });
